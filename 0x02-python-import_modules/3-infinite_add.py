3f8baa9ff888f9d3f75c4e66e1255ffe04b5b03c
Nnamani John <nnamanijoh123@gmail.com>
2022-12-02 02:05:30 +0000

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
 
-    if len(argv) > 2:
-        sum = 0
-        for i in range(1, len(argv)):
-            sum += int(argv[i])
-        print(sum)
+    sum = 0
+    for i in range(1, len(argv)):
+        sum += int(argv[i])
+    print("{}".format(sum))
