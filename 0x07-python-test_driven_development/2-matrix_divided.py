3998e7c69c8f344199217694ae2bfe945eb95aa8
Nnamani John <nnamanijoh123@gmail.com>
2022-12-30 23:05:51 +0000

    matrix divided

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,6 +9,8 @@ def matrix_divided(matrix, div):
     """
     function that divides all elements of a matrix.
     """
+    if type(matrix) != list or len(matrix) == 0:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     for row in matrix:
         if type(row) != list:
             raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
