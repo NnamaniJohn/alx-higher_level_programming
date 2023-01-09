d43b7f9cf590e74fce6eb420640bcdb73f42ce0c
Nnamani John <nnamanijoh123@gmail.com>
2023-01-09 15:07:33 +0000

    area exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""base geometry module contains class BaseGeometry"""
+
+
+class BaseGeometry:
+    """geometry class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
