6f238bf833f12649afbc36b910a1e600683092c1
Nnamani John <nnamanijoh123@gmail.com>
2023-01-09 23:23:27 +0000

    add attribute

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,6 +2,9 @@
 
 """add attribute module"""
 def add_attribute(obj, name, value):
+    """
+    function that adds a new attribute to an object
+    """
     try:
         obj.__setattr__(name, value)
     except Exception:
