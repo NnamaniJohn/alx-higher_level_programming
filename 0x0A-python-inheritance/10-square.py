0b8153d26c97b60350d65943bd3cdcf649ff2b34
Nnamani John <nnamanijoh123@gmail.com>
2023-01-09 16:02:36 +0000

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""square module contains class Square"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A Square class"""
+
+    def __init__(self, size):
+        super().__init__(size, size)
+        try:
+            self.integer_validator("size", size)
+            self.__size = size
+        except Exception as e:
+            raise e
+
+    def area(self):
+        return self.__size * self.__size
