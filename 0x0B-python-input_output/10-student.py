25553b81a185dfb729bf77ef2077e6b02450dd9a
Nnamani John <nnamanijoh123@gmail.com>
2023-01-11 02:18:00 +0000

    student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""student module"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs:
+            return dict(filter(lambda ele: ele[0] in attrs, self.__dict__.items()))
+        else:
+            return self.__dict__
