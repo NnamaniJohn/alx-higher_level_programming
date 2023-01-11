6336d57bf17d6d7d8cf8797e50e0824314717a62
Nnamani John <nnamanijoh123@gmail.com>
2023-01-11 03:33:59 +0000

    append after

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""append after"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    text = ""
+    with open(filename, encoding="utf-8") as myFile:
+        while True:
+            line = myFile.readline()
+            if line == '':
+                break;
+            text += line
+            if search_string in line:
+                text += new_string
+    
+    with open(filename, mode="w", encoding="utf-8") as myFile:
+        myFile.write(text)
