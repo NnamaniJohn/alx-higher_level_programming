010398a7562b2aeec4c0a00dbb6191c744569420
Nnamani John <nnamanijoh123@gmail.com>
2023-02-16 15:35:36 +0000

    groups

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- number by score
-SELECT score, COUNT(score) as number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY number DESC;
